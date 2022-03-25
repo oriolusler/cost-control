fb62af2962ca0de65b2134944b0e39822fa8f217
Oriol Soler <oriol.soler@hotmail.com>
2022-03-25 09:20:48 +0100

    HelloWorld Controller

---
+++ src/main/kotlin/com/oriolsoler/costcontroler/CostControlerApplication.kt
@@ -0,0 +1,11 @@
+package com.oriolsoler.costcontroler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CostControlerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CostControlerApplication>(*args)
+}
