b034ef6855f22940f2e1a0ba311493a84d0d01a8
Oriol Soler <oriol.soler@adevinta.com>
2022-09-01 19:57:57 +0200

    Removing inncessary cide

--- src/main/resources/static/js/get_cost.js
+++ src/main/resources/static/js/get_cost.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
     const categoriesSelector = document.getElementById("categoryCostGet");
 
     const subcategoriesSelector = document.getElementById("subcategoryCostGet");
