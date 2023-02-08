c331bfb2067f653d9448f8a246561e60db1aaa1f
Oriol Soler <oriol.soler@adevinta.com>
2023-02-08 20:44:18 +0100

    Global month status colors

--- src/main/resources/static/js/acquisitions.js
+++ src/main/resources/static/js/acquisitions.js
@@ -20,20 +20,48 @@ function getGeneralMonthBalance() {
 }
 
 new Chart(document.getElementById('acquisitions'), {
-    type: 'bar', data: {
-        labels: generalMonthBalance.map(row => row.status), datasets: [{
-            label: 'Overall', data: generalMonthBalance.map(row => row.value)
-        }]
+        type: 'bar',
+        data: {
+            datasets: [
+                {
+                    label: 'Earned',
+                    data: [income],
+                    borderColor: "#4bb6fa",
+                    backgroundColor: "#9cd0f6",
+                    borderWidth: 2
+                },
+                {
+                    label: 'Spent',
+                    data: [expenses],
+                    borderColor: "#fa2e2e",
+                    backgroundColor: "#fc8e9c",
+                    borderWidth: 2
+                },
+                {
+                    label: 'Invest',
+                    data: [invested],
+                    borderColor: "#2ec931",
+                    backgroundColor: "#c4ffa5",
+                    borderWidth: 2
+                }
+            ]
+        }
     }
-});
+);
 
 
 new Chart(document.getElementById('acquisitions1'), {
-    type: 'bar', data: {
-        labels: categoryMonthBalance.map(row => row.category), datasets: [{
-            label: 'By categories', data: categoryMonthBalance.map(row => row.value)
-        }]
+        type: 'bar',
+        data: {
+            labels: categoryMonthBalance.map(row => row.category),
+            datasets: [
+                {
+                    label: 'By categories',
+                    data: categoryMonthBalance.map(row => row.value)
+                }
+            ]
+        }
     }
-});
+);
 
 
