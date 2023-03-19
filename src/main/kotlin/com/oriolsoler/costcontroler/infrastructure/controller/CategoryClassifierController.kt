6d8ea28c7003d0ea9417477bfdc9c22a194da5d6
Oriol Soler <oriol.soler@adevinta.com>
2023-03-19 12:18:50 +0100

    Fixing get file

--- src/main/kotlin/com/oriolsoler/costcontroler/infrastructure/controller/CategoryClassifierController.kt
+++ src/main/kotlin/com/oriolsoler/costcontroler/infrastructure/controller/CategoryClassifierController.kt
@@ -1,10 +1,14 @@
 package com.oriolsoler.costcontroler.infrastructure.controller
 
 import com.oriolsoler.costcontroler.infrastructure.services.NaiveBayesModel
+import org.apache.tomcat.util.http.fileupload.IOUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+
 
 @RestController
 class CategoryClassifierController {
@@ -13,10 +17,17 @@ class CategoryClassifierController {
 
     @GetMapping("/expense/category/classifier")
     fun classifyCategory(@RequestParam expense: String): ClassifierResponse {
-        nbyCategory.trainModel(File(this::class.java.classLoader.getResource("data/categories_data.txt").toURI()))
-        nbySubategory.trainModel(File(this::class.java.classLoader.getResource("data/subcategories_data.txt").toURI()))
+        nbyCategory.trainModel(stream2file(this::class.java.classLoader.getResourceAsStream("data/categories_data.txt")))
+        nbySubategory.trainModel(stream2file(this::class.java.classLoader.getResourceAsStream("data/subcategories_data.txt")))
 
         return ClassifierResponse(nbyCategory.categorizeExpense(expense), nbySubategory.categorizeExpense(expense))
+    }
+
+    fun stream2file(input: InputStream?): File {
+        val tempFile = File.createTempFile("temp", ".txt")
+        tempFile.deleteOnExit()
+        FileOutputStream(tempFile).use { out -> IOUtils.copy(input, out) }
+        return tempFile
     }
 }
 
